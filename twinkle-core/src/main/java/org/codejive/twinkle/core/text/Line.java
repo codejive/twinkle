9acd0382cbb250154db4b6a5f227e8780497cd73
Tako Schotanus <tako@codejive.org>
2025-12-19 00:36:22 +0100

    feat: added `Span` and `Line`

---
+++ twinkle-core/src/main/java/org/codejive/twinkle/core/text/Line.java
@@ -0,0 +1,41 @@
+package org.codejive.twinkle.core.text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.codejive.twinkle.ansi.Style;
+import org.codejive.twinkle.core.widget.Canvas;
+import org.codejive.twinkle.core.widget.Renderable;
+
+public class Line implements Renderable {
+    private final List<Span> spans;
+
+    public static Line of(String text) {
+        return new Line((Span.of(text)));
+    }
+
+    public static Line of(String text, Style style) {
+        return of(text, style.state());
+    }
+
+    public static Line of(String text, long styleState) {
+        return new Line((Span.of(text, styleState)));
+    }
+
+    public static Line of(Span... spans) {
+        return new Line(spans);
+    }
+
+    protected Line(Span... spans) {
+        this.spans = new ArrayList<>(spans.length);
+        Collections.addAll(this.spans, spans);
+    }
+
+    @Override
+    public void render(Canvas canvas) {
+        int x = 0;
+        for (Span span : spans) {
+            span.render(canvas.view(x, 0, span.length(), 1));
+        }
+    }
+}
---
+++ twinkle-core/src/main/java/org/codejive/twinkle/core/text/Span.java
@@ -0,0 +1,38 @@
+package org.codejive.twinkle.core.text;
+
+import org.codejive.twinkle.ansi.Style;
+import org.codejive.twinkle.core.widget.Canvas;
+import org.codejive.twinkle.core.widget.Renderable;
+
+public class Span implements Renderable {
+    private final String text;
+    private final long styleState;
+    private final int length;
+
+    public static Span of(String text) {
+        return new Span(text, Style.F_UNSTYLED);
+    }
+
+    public static Span of(String text, Style style) {
+        return new Span(text, style.state());
+    }
+
+    public static Span of(String text, long styleState) {
+        return new Span(text, styleState);
+    }
+
+    protected Span(String text, long styleState) {
+        this.text = text;
+        this.styleState = styleState;
+        this.length = text.codePointCount(0, text.length());
+    }
+
+    public int length() {
+        return length;
+    }
+
+    @Override
+    public void render(Canvas canvas) {
+        canvas.putStringAt(0, 0, styleState, text);
+    }
+}
