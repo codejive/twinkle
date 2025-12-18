feb36f188032d7b3a2f7190ebc3809571446a885
Tako Schotanus <tako@codejive.org>
2025-12-19 00:41:40 +0100

    chore: fixed math demo and made it framed

--- twinkle-chart/src/test/java/examples/MathPlotDemo.java
+++ twinkle-chart/src/test/java/examples/MathPlotDemo.java
@@ -1,14 +1,19 @@
 package examples;
 
+import org.codejive.twinkle.core.text.Line;
 import org.codejive.twinkle.core.widget.Panel;
+import org.codejive.twinkle.core.widget.Size;
+import org.codejive.twinkle.widgets.Framed;
 import org.codejive.twinkle.widgets.graphs.plot.MathPlot;
 
 public class MathPlotDemo {
     public static void main(String[] args) {
-        Panel pnl = Panel.of(40, 20);
-        MathPlot p = MathPlot.of(pnl).ranges(-2 * Math.PI, 2 * Math.PI, -1.0, 1.0);
+        MathPlot p = MathPlot.of(Size.of(40, 20)).ranges(-2 * Math.PI, 2 * Math.PI, -1.0, 1.0);
         // plot a sine wave
         p.plot(Math::sin);
-        System.out.println(pnl.toString());
+        Framed f = Framed.of(p).title(Line.of(" Sine Wave "));
+        Panel pnl = Panel.of(42, 22);
+        f.render(pnl);
+        System.out.println(pnl);
     }
 }
