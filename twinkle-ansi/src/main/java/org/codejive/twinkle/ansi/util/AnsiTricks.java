91492a6c6733001ed868fdec2902421c13a4a5de
Tako Schotanus <tako@codejive.org>
2026-03-14 18:17:05 +0100

    feat: added `Sizer` class for measuring texts
    Also added an overload for `AnsiTricks.blockify()`

--- twinkle-ansi/src/main/java/org/codejive/twinkle/ansi/util/AnsiTricks.java
+++ twinkle-ansi/src/main/java/org/codejive/twinkle/ansi/util/AnsiTricks.java
@@ -11,6 +11,19 @@ public class AnsiTricks {
      * cursor will each time be moved down from the starting point of the previous line, so that the
      * text will be printed in a block.
      *
+     * @param text the text to be blockified
+     */
+    public static String blockify(@NonNull String text) {
+        StringBuilder sb = new StringBuilder();
+        blockify(sb, text);
+        return sb.toString();
+    }
+
+    /**
+     * Appends the given text to the appendable. When the text consists of multiple lines, the
+     * cursor will each time be moved down from the starting point of the previous line, so that the
+     * text will be printed in a block.
+     *
      * @param appendable the Appendable to which the text will be appended
      * @param text the text to be blockified
      */
@@ -19,6 +32,12 @@ public class AnsiTricks {
             String[] parts = text.split("(\r)?\n");
             for (int i = 0; i < parts.length; i++) {
                 if (i > 0) {
+                    // We keep the linefeed for two reasons:
+                    // - we might need the screen to scroll up, something
+                    //    cursorDown won't do
+                    // - the caller might still want to know how many lines
+                    //   there are in the text by splitting on newlines
+                    appendable.append('\n');
                     appendable.append(Ansi.cursorRestore());
                     appendable.append(Ansi.cursorDown(1));
                 }
---
+++ twinkle-text/src/test/java/org/codejive/twinkle/text/util/TestSizer.java
@@ -0,0 +1,62 @@
+package org.codejive.twinkle.text.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.codejive.twinkle.ansi.Ansi;
+import org.codejive.twinkle.ansi.Constants;
+import org.junit.jupiter.api.Test;
+
+public class TestSizer {
+
+    @Test
+    public void testMeasureEmptyString() {
+        assertThat(Sizer.measure("")).isEqualTo(Size.of(0, 0));
+    }
+
+    @Test
+    public void testMeasureSingleLineString() {
+        assertThat(Sizer.measure("hello")).isEqualTo(Size.of(5, 1));
+    }
+
+    @Test
+    public void testMeasureWithSeveralLineFeeds() {
+        assertThat(Sizer.measure("ab\nc\ndef")).isEqualTo(Size.of(3, 3));
+    }
+
+    @Test
+    public void testMeasureWithLineFeedsOnEmptyLines() {
+        assertThat(Sizer.measure("\nalpha\n\n")).isEqualTo(Size.of(5, 4));
+    }
+
+    @Test
+    public void testMeasureIgnoresAnsiEscapeSequenceWidth() {
+        String red = Constants.CSI + "31m";
+        String reset = Ansi.STYLE_RESET;
+
+        assertThat(Sizer.measure(red + "red" + reset)).isEqualTo(Size.of(3, 1));
+    }
+
+    @Test
+    public void testMeasureIgnoresAnsiEscapeSequenceWidthAcrossLines() {
+        String green = Constants.CSI + "32m";
+        String reset = Ansi.STYLE_RESET;
+        String text = "A" + green + "B" + reset + "\n\n" + green + "CD" + reset;
+
+        assertThat(Sizer.measure(text)).isEqualTo(Size.of(2, 3));
+    }
+
+    @Test
+    public void testMeasureWithOnlyWhitespaceLines() {
+        assertThat(Sizer.trim("   \n\t\n ")).isEqualTo("");
+    }
+
+    @Test
+    public void testMeasureTrimsEachLine() {
+        assertThat(Sizer.trim("  a  \n  b  ")).isEqualTo("a\nb");
+    }
+
+    @Test
+    public void testMeasurePreservesInteriorEmptyLinesAfterTrimming() {
+        assertThat(Sizer.trim("  one\n   \n two  ")).isEqualTo("one\n\ntwo");
+    }
+}
